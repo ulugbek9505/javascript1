13c6ed160e02674697aa991e4caf543aeea99152
ulugbek9505 <mtvazirligi@gmail.com>
2024-12-14 00:35:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let name = prompt("Ismingni kiriting:");
+let surname = prompt("Familyangiz kiriting:");
+let age = prompt("Yoshingiz kiriting:");
+let Symbol = prompt("Yashaydigan tumaningiz kiriting");
+
+console.log("Sizning ismingiz: " + name);
+console.log("Sizning familyangiz: " + surname);
+console.log("Sizning yoshingiz: " + age);
+console.log("Yashaydigan tumaningiz kiriting:" + Symbol);
+
+
